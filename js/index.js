42f8b1a71a48b929914247acd90e4fbbfcb14632
Ziad Khaled <ziadkhed17@gmail.com>
2024-11-07 23:50:54 +0200

    Upload The Design

---
+++ js/index.js
@@ -0,0 +1,24 @@
+var typed = new Typed('#element', {
+    strings: ['Developer', 'Designer', 'Freelancer',' Larry Daniels'],
+    typeSpeed: 100,
+    backSpeed:100,
+    loop:true,
+  });
+
+ AOS.init();
+
+
+//=========== start navbar scroll======//
+// function changeBg(){
+//   var navbar =document.getElementById('navbar')
+//   var scrollValue = window.scrollY;
+//   if(scrollValue < 150){
+//     navbar.classList.remove('bgcolor')
+//   }
+//   else{
+//     navbar.classList.add('bgcolor')
+//   }
+//   // console.log(scrollValue);
+// }
+// window.addEventListener('scroll' , changeBg)
+//=========== End navbar scroll======//
